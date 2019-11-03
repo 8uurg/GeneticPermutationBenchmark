9ab7b22f6d2dfb4b9e37ae15968e080634acc1ef
8uurg <arthur.guijt12@gmail.com>
2019-11-03 17:03:32 +0100

    Add some utilities related to the QAP

--- src/problems/QAP/qap.jl
+++ src/problems/QAP/qap.jl
@@ -3,6 +3,7 @@
 # !!!note The Quadratic Assignment Problem is a **minimization** problem.
 #         As such the bb_wrap_qap function negates the objective value
 #         to allow maximization to work.
+
 "A struct representing an instance of the Quadratic Assignment Problem"
 struct QAPInstance{T <: Real}
     n :: Int64
@@ -59,3 +60,109 @@ function parse_qap_qaplib(T, data :: String) :: QAPInstance{T}
     B = collect(reshape(parse.(Ref(T), data[2+n*n:1+2*n*n]), (n, n))')
     QAPInstance{T}(n, A, B)
 end
+
+"""
+Relabel the A matrix of a Quadratic Assignment Problem instance.
+
+Note: this changes the meaning of a solution. 
+A solution `s` in for the unrelabeled `instance` is permutated by the the assignment's.
+hence `s[assignment]` yields a solution with the same objective value.
+"""
+function relabel_A(instance :: QAPInstance, assignment :: Vector{Int64})
+    QAPInstance(instance.n, instance.A[assignment, assignment], instance.B)
+end
+
+"""
+Relabel the B matrix of a Quadratic Assignment Problem instance.
+
+Note: this changes the meaning of a solution.
+A solution `s` in for the unrelabeled `instance` is permutated by the the assignment's inverse.
+hence `s[invperm(assignment)]` yields a solution with the same objective value.
+If `assignment` was the optimal solution then the new optimal solution is `[1, ..., instance.n]`.
+"""
+function relabel_B(instance :: QAPInstance, assignment :: Vector{Int64})
+    QAPInstance(instance.n, instance.A, instance.B[assignment, assignment])
+end
+ 
+"""
+Relabel the A matrix of a Quadratic Assignment Problem instance.
+
+Note: this changes the meaning of a solution. 
+A solution `s` in for the unrelabeled `instance` is permutated by the the assignment's inverse.
+If `assignment` was the optimal solution then
+the new optimal solution is `[1, ..., instance.n]`.
+"""
+function relabel_inv_A(instance :: QAPInstance, assignment :: Vector{Int64})
+    iassignment = invperm(assignment)
+    QAPInstance(instance.n, instance.A[iassignment, iassignment], copy(instance.B))
+end
+
+"""
+Relabel the B matrix of a Quadratic Assignment Problem instance.
+
+Note: this changes the meaning of a solution.
+A solution `s` in for the unrelabeled `instance` is permutated by the the assignment's.
+
+"""
+function relabel_inv_B(instance :: QAPInstance, assignment :: Vector{Int64})
+    iassignment = invperm(assignment)
+    QAPInstance(instance.n, copy(instance.A), instance.B[iassignment, iassignment])
+end
+
+"""
+    qap_distance_weight_matrix(n :: Int64)
+
+Create a symmetric n × n matrix where each position equals the distance
+between the two positions. 
+"""
+function qap_distance_weight_matrix(n :: Int64)
+    m = zeros(Int64, (n, n))
+    for r in 1:n, c in 1:n
+        m[r, c] = abs(r - c)
+    end
+    return m
+end
+
+
+"""
+Create a symmetric n × n matrix where each position equals n - the distance
+between the two positions. 
+"""
+function qap_inverted_distance_weight_matrix(n :: Int64)
+    m = zeros(Int64, (n, n))
+    for r in 1:n, c in 1:n
+        m[r, c] = n - abs(r - c)
+    end
+    return m
+end
+
+"""
+    qap_bidirectional_path_weight_matrix(n :: Int64)
+
+Construct a weight matrix corresponding to finding the shortest (bidirectional)
+Hamiltonian path (a path that visits all vertices exactly once).
+"""
+function qap_bidirectional_path_weight_matrix(n :: Int64)
+    m = zeros(Int64, (n, n))
+    for r in 1:(n-1)
+        m[r, r+1] = 1
+        m[r+1, r] = 1
+    end
+    return m
+end
+
+"""
+    qap_tsp_weight_matrix(n :: Int64)
+
+Construct a weight matrix corresponding to the Travelling Salesperson Problem
+"""
+function qap_tsp_weight_matrix(n :: Int64)
+    m = zeros(Int64, (n, n))
+    for r in 1:(n-1)
+        m[r, r+1] = 1
+    end
+    m[n, 1] = 1
+    return m
+end
+
+# p = optimize_gomea(bb_wrap_qap(QAPInstance(insta.n, qap_inverted_distance_weight_matrix(insta.n), copy(insta.A))), insta.n, 10.0; forced_improvement=:extended)
