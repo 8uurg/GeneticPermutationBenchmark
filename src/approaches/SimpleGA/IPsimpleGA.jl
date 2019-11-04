65116dfc35974331cfa7a4b353375e67dc3d34cd
8uurg <arthur.guijt12@gmail.com>
2019-11-04 09:32:47 +0100

    Create variant that creates both offspring

--- src/approaches/SimpleGA/IPsimpleGA.jl
+++ src/approaches/SimpleGA/IPsimpleGA.jl
@@ -39,6 +39,42 @@ function crossover_PMX(dst :: Vector{Int64}, src :: Vector{Int64}, idst :: Vecto
     return dst
 end
 
+function crossover_PMX_both!(dst1 :: Vector{Int64}, dst2 :: Vector{Int64}, parent1 :: Vector{Int64}, parent2 :: Vector{Int64}, 
+        idst1 :: Vector{Int64}, idst2 :: Vector{Int64}; ring :: Bool = true)
+    n = length(parent1)
+    #
+    copy!(dst1, parent1)
+    copy!(dst2, parent2)
+    # Build the inverse permutation for use as a lookup table.
+    # Effectively working as a find-city but it does need to be kept up-to-date with each swap.
+    invperm!(idst1, dst1)
+    invperm!(idst2, dst2)
+    # Pick two points on the string
+    ms_a, ms_b = rand(1:n), rand(1:n)
+    # If not acting like a ring, do a swap if neccesary.
+    if !ring && ms_b < ms_a
+        ms_a, ms_b = ms_b, ms_a
+    end
+    # These two points from a section on the string (which is assumed to be a ring)
+    # eg. if b < a, it wraps around the end.
+    if ms_b >= ms_a
+        for i in ms_a:ms_b
+            crossover_PMX_point(dst1, parent2, idst1, i)
+            crossover_PMX_point(dst2, parent1, idst2, i)
+        end
+    else
+        for i in ms_a:n
+            crossover_PMX_point(dst1, parent2, idst1, i)
+            crossover_PMX_point(dst2, parent1, idst2, i)
+        end
+        for i in 1:ms_b
+            crossover_PMX_point(dst1, parent2, idst1, i)
+            crossover_PMX_point(dst2, parent1, idst2, i)
+        end
+    end
+    return dst1, dst2
+end
+
 function crossover_CX(dst :: Vector{Int64}, src :: Vector{Int64}, idst :: Vector{Int64}, isrc :: Vector{Int64})
     # Create lookup tables for finding cycles.
     invperm!(idst, dst)
