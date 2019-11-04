c2784aa86bab08cee1d734f44cd595b60fe868e6
8uurg <arthur.guijt12@gmail.com>
2019-11-04 10:15:51 +0100

    Add order crossover implementation

--- src/approaches/SimpleGA/IPsimpleGA.jl
+++ src/approaches/SimpleGA/IPsimpleGA.jl
@@ -73,3 +73,78 @@ function crossover_CX!(dst1 :: Vector{Int64}, dst2 :: Vector{Int64}, parent1 :: 
     
     return dst1, dst2
 end
+
+function crossover_OX!(dst1 :: Vector{Int64}, dst2 :: Vector{Int64}, parent1 :: Vector{Int64}, parent2 :: Vector{Int64}, 
+        seen_dst1 :: BitVector, seen_dst2 :: BitVector)
+    n = length(parent1)
+    # Select crossover points
+    ms_a, ms_b = rand(1:n), rand(1:n)
+    # Keep track of which items we have seen
+    fill!(seen_dst1, false)
+    fill!(seen_dst2, false)
+    # Case distinction between wrapping around and
+    # non-wraparound.
+    if ms_b >= ms_a
+        # Copy over elements in window
+        for i in ms_a:ms_b
+            dst1[i] = parent1[i]
+            seen_dst1[parent1[i]] = true
+            dst2[i] = parent2[i]
+            seen_dst2[parent2[i]] = true
+        end
+        # From other parent, copy over elements
+        # for positions outside of window.
+        p1 = 1
+        p2 = 1
+        for i in 1:ms_a-1
+            # Ignore seen elements.
+            while seen_dst2[parent1[p1]]
+                p1 += 1
+            end
+            while seen_dst1[parent2[p2]]
+                p2 += 1
+            end
+            dst2[i] = parent1[p1]
+            dst1[i] = parent2[p2]
+        end
+        for i in ms_b+1:n
+            # Ignore seen elements.
+            while seen_dst2[parent1[p1]]
+                p1 += 1
+            end
+            while seen_dst1[parent2[p2]]
+                p2 += 1
+            end
+            dst2[i] = parent1[p1]
+            dst1[i] = parent2[p2]
+        end
+    else
+        # Copy over elements in window
+        for i in ms_a:n
+            dst1[i] = parent1[i]
+            seen_dst1[parent1[i]] = true
+            dst2[i] = parent2[i]
+            seen_dst2[parent2[i]] = true
+        end
+        for i in 1:ms_b
+            dst1[i] = parent1[i]
+            seen_dst1[parent1[i]] = true
+            dst2[i] = parent2[i]
+            seen_dst2[parent2[i]] = true
+        end
+        p1 = 1
+        p2 = 1
+        for i in ms_a+1:ms_b-1
+            # Ignore seen elements.
+            while seen_dst2[parent1[p1]]
+                p1 += 1
+            end
+            while seen_dst1[parent2[p2]]
+                p2 += 1
+            end
+            dst2[i] = parent1[p1]
+            dst1[i] = parent2[p2]
+        end
+    end
+    return dst1, dst2
+end
