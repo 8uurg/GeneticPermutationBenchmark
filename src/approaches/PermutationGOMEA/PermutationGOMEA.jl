1cd18597bc45d0eebe861ca19073f10fbd60d160
8uurg <arthur.guijt12@gmail.com>
2020-01-17 16:01:15 +0100

    Fix

--- src/approaches/PermutationGOMEA/PermutationGOMEA.jl
+++ src/approaches/PermutationGOMEA/PermutationGOMEA.jl
@@ -1,18 +1,21 @@
 using GomeaWrapper
 
-function wrap_objective(f :: Function)
+function wrap_objective(f :: Function, n :: Int64)
+    perm = collect(1:n)
     function to_multiple(x :: Vector{Float64})
-        return f(x), 0.0
+        sortperm!(perm, x)
+        return f(perm), 0.0
     end
     return to_multiple
 end
 
-function optimize_permutationgomea(func :: Function, n :: Int64, t :: Int64, e :: Int64, 
-    fos_type :: Symbol, reencode=true, rescale=true)
+function optimize_permutationgomea(func :: Function, n :: Int64, t :: Float64, e :: Int64 = -1; 
+    fos_type :: Symbol = :original, reencode=true, rescale=true)
     #
     fosStructure = :linkage_tree
     if fos_type == :univariate
         fosStructure = :univariate
     end
-    optimizeGOMEA(wrap_objective(func), n, t, e, fosStructure, reencode=reencode, rescale=rescale)
+    obj, constr, best = optimizeGOMEA(wrap_objective(func, n), n; maxTime=convert(Int64, t*1000), maxEvaluations=e, fosStructure=fosStructure, dependency_matrix_approach=fos_type, reencode=reencode, rescale=rescale)
+    (obj, sortperm(best))
 end
