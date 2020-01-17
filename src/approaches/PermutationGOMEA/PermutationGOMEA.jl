bc25794304ec879a5362a6725222b8b26df315ed
8uurg <arthur.guijt12@gmail.com>
2020-01-17 16:16:05 +0100

    Add handler for Infinite time

--- src/approaches/PermutationGOMEA/PermutationGOMEA.jl
+++ src/approaches/PermutationGOMEA/PermutationGOMEA.jl
@@ -16,6 +16,10 @@ function optimize_permutationgomea(func :: Function, n :: Int64, t :: Float64, e
     if fos_type == :univariate
         fosStructure = :univariate
     end
-    obj, constr, best = optimizeGOMEA(wrap_objective(func, n), n; maxTime=convert(Int64, t*1000), maxEvaluations=e, fosStructure=fosStructure, dependency_matrix_approach=fos_type, reencode=reencode, rescale=rescale)
+    conv_t = -1
+    if t != Inf
+        convert(Int64, t*1000)
+    end
+    obj, constr, best = optimizeGOMEA(wrap_objective(func, n), n; maxTime=conv_t, maxEvaluations=e, fosStructure=fosStructure, dependency_matrix_approach=fos_type, reencode=reencode, rescale=rescale)
     (obj, sortperm(best))
 end
