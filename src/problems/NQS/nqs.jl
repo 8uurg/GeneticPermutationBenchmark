ad4eb2584607c19a78f9cdc46f850d2eadf99cf3
8uurg <arthur.guijt12@gmail.com>
2020-01-21 19:29:12 +0100

    Add variation to NQueens evaluation

--- src/problems/NQS/nqs.jl
+++ src/problems/NQS/nqs.jl
@@ -26,10 +26,40 @@ function evaluate_nqs(instance :: NQSInstance, solution :: Vector{Int64}, diag1_
     return score
 end
 
+function evaluate_nqs2(instance :: NQSInstance, solution :: Vector{Int64}, diag1_set :: Vector{Int64}, diag2_set :: Vector{Int64})
+    # Constants
+    n = length(solution)
+    @assert n == instance.n
+    # Prepare memory structures
+    resize!(diag1_set, n*2-1)
+    resize!(diag2_set, n*2-1)
+    fill!(diag1_set, 0)
+    fill!(diag2_set, 0)
+    #
+    score = 0
+    # 
+    for (idx, e) in enumerate(solution)
+        diag1_idx = idx - e + n
+        diag2_idx = idx + e - 1
+        score += diag1_set[diag1_idx] + diag2_set[diag2_idx]
+        diag1_set[diag1_idx] += 1
+        diag2_set[diag2_idx] += 1
+    end
+    return score
+end
+
 function bb_wrap_nqs(insta :: NQSInstance)
     diag1_set :: BitVector = falses(insta.n)
     diag2_set :: BitVector = falses(insta.n)
     function evaluate(perm :: Vector{Int64})
         return -evaluate_nqs(insta, perm, diag1_set, diag2_set)
+    end
+end
+
+function bb_wrap_nqs2(insta :: NQSInstance)
+    diag1_set :: Vector{Int64} = zeros(insta.n)
+    diag2_set :: Vector{Int64} = zeros(insta.n)
+    function evaluate(perm :: Vector{Int64})
+        return -evaluate_nqs2(insta, perm, diag1_set, diag2_set)
     end
 end
