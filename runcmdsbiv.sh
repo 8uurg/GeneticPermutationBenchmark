78619aeeb8d79382a118e118b7991b5570d7453e
8uurg <arthur.guijt12@gmail.com>
2021-07-20 15:30:41 +0200

    Set up experiments for re-running GOMEAs
    Due to an error the versions of GOMEA implemented in Julia
    have been using a steady-state like variant. This is not how
    the original Permutation GOMEA - nor qGOMEA was supposed
    to work. A previous commit fixed this issue.
    This commit contains the required scripts to re-run experiments
    which were affected by this fix (all experiments involving GOMEA).
    Furthermore, as this re-run will be completely performed on a server,
    all runs for BIV (the inversions benchmark functions) will be re-ran again,
    as otherwise hardware differences make the wall-time limit questionable.

---
+++ runcmdsbiv.sh
@@ -0,0 +1,13 @@
+export JULIA_NUM_THREADS = 16
+
+echo "Running BIV incrementally for all"
+
+# Part of the re-run on due to
+# GOMEA due to inconsistency (implementation is steady state, while it shouldn't be)
+# However, as this will be ran on a different machine,
+# all approaches will be re-ran instead.
+
+cat ./cmdsbiv.txt | xargs -P 0 -n 1 -d '\n' sh -c
+# TODO: Add more copy commands for other problems - if needed
+mkdir -p ./results/BIV && cp -r ./src/problems/BIV/results/* ./results/BIV/
+tar -czf ./results.tar.gz ./results
---
+++ runcmdsoas_rerun_gomea.sh
@@ -0,0 +1,7 @@
+export JULIA_NUM_THREADS = 16
+# Rerunning GOMEA due to inconsistency (implementation is steady state, while it shouldn't be)
+
+cat ./cmdsoas_rerun_gomea.txt | xargs -P 1 -n 1 -d '\n' sh -c
+# TODO: Add more copy commands for other problems - if needed
+mkdir -p ./results/OAS && cp -r ./src/problems/OAS/results/* ./results/OAS/
+tar -czf ./results.tar.gz ./results
