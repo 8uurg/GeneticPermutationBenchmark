d3b3949a807faa501f36bd3d2d28228993b5da7d
8uurg <arthur.guijt12@gmail.com>
2021-07-30 10:10:52 +0200

    Update constructor for use with DataFrames 1.x

--- src/problems/PFS/run_single_taillard_per_cat_eval_limit_focus.jl
+++ src/problems/PFS/run_single_taillard_per_cat_eval_limit_focus.jl
@@ -143,11 +143,11 @@ sort!(instances)
 
 # Initialize results storage
 results_time = DataFrame(
-    [   String,    String,  Int64, Float64,    Float64], 
-    [:instance, :approach, :exp_i,   :time, :objective])
+    [Vector{String}(), Vector{String}(), Vector{Int64}(), Vector{Float64}(), Vector{Float64}()], 
+    [       :instance,        :approach,          :exp_i,             :time,        :objective])
 results_eval = DataFrame(
-    [   String,    String,  Int64,        Int64,    Float64], 
-    [:instance, :approach, :exp_i, :evaluations, :objective])
+    [Vector{String}(), Vector{String}(), Vector{Int64}(), Vector{Int64}(), Vector{Float64}()], 
+    [       :instance,        :approach,          :exp_i,    :evaluations,        :objective])
 
 begin
     println("Warming up approach")
