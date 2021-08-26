78aba8102f6866fd3ed98ff56522e11a98f8549b
8uurg <arthur.guijt12@gmail.com>
2021-08-26 15:03:23 +0200

    [!] Set up experiment to run remainder
    WARNING: running `generate_cmdspfs_rerun_left.py`
    will overwrite `cmdspfs_rerun_all.txt`.

---
+++ generate_cmdspfs_rerun_left.py
@@ -0,0 +1,13 @@
+from itertools import product
+
+start_indices = [1]
+instance_ids = range(80, 120 + 1)
+approach_ids = [1, 2, 3, 4, 5, 6, 7, 8]
+num_exp = 20
+
+def generate_run_cmd(start_idx, instance_id, approach_id, num_exp):
+    return f"cd ./src/problems/PFS && julia -O3 --project=../../.. ./run_single_taillard_per_cat_eval_limit_focusrrg.jl {start_idx} {instance_id} {approach_id} {num_exp}\n"
+
+with open("./cmdspfs_rerun_all.txt", "w") as f:
+    for (start_idx, instance_id, approach_id) in product(start_indices, instance_ids, approach_ids):
+        f.write(generate_run_cmd(start_idx, instance_id, approach_id, num_exp))
